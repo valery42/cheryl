8ebe44ec35dfa58f66ec8c53b9de499830e29559
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-03-27 18:25:08 +0300

    Add test helpers

---
+++ test/helpers.py
@@ -0,0 +1,14 @@
+def get_record():
+    record = "'111-222-333','Martin Eden','Jack London','Penguin','555'"
+    return record
+
+
+def get_book():
+    book = {
+        "isbn": "111-222-333",
+        "title": "Martin Eden",
+        "author": "Jack London",
+        "publisher": "Penguin",
+        "pages": 555,
+    }
+    return book
