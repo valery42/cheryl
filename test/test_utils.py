bd9e1d2ffe6d66a68d12be52e4f2c638b0a88486
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-03-28 19:18:20 +0300

    Test get utils

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,4 @@
-from cheryl.utils import quote, unquote
+from cheryl.utils import quote, unquote, get_prompt
 
 
 def test_quote():
@@ -9,3 +9,8 @@ def test_quote():
 def test_unquote():
     item = "'42'"
     assert unquote(item) == "42"
+
+
+def test_get_prompt():
+    prompt = get_prompt(message="hello", gaps=1)
+    assert prompt == "    hello> "
