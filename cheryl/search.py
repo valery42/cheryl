b97357da0325772437ebd035172f2b2595ec0a77
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-03-29 18:02:02 +0300

    Search in lower case

--- cheryl/search.py
+++ cheryl/search.py
@@ -2,13 +2,14 @@ from cheryl.config import UNSUCCESSFUL
 
 
 def binary_search(books, *, key, target):
+    target = target.lower()
     left = 0
     right = len(books) - 1
     while left <= right:
         middle = (left + right) // 2
-        if books[middle][key] < target:
+        if books[middle][key].lower() < target:
             left = middle + 1
-        elif books[middle][key] > target:
+        elif books[middle][key].lower() > target:
             right = middle - 1
         else:
             return middle
