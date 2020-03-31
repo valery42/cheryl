536ef6ce9ff8045d0ddfb9f7f485d72d796813ad
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-03-31 18:52:40 +0300

    Document checkers module

--- cheryl/checkers.py
+++ cheryl/checkers.py
@@ -1,13 +1,17 @@
+"""This module contains checkers for isbn and pages."""
+
 from cheryl.config import (
     MIN_PAGES, MAX_PAGES, ISBN_SEP, ISBN_NUM_VALUES, ISBN_VALUE_LENGTH
 )
 
 
-def is_correct_pages(pages):
+def is_correct_pages(pages: str) -> bool:
+    """Check if pages is integer in proper limits."""
     return pages.isnumeric() and MIN_PAGES <= int(pages) <= MAX_PAGES
 
 
-def is_correct_isbn(isbn):
+def is_correct_isbn(isbn: str) -> bool:
+    """Check if isbn follows ISBN_PATTERN."""
     values = isbn.split(ISBN_SEP)
     if len(values) != ISBN_NUM_VALUES:
         return False
