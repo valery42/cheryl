63f0fb28e8b5ee78e4306ffb088f9582a55d49c4
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-03-31 19:51:17 +0300

    Document search module

--- cheryl/search.py
+++ cheryl/search.py
@@ -1,7 +1,13 @@
+"""This module contains binary search algorithm implementation."""
+
 from cheryl.config import UNSUCCESSFUL
 
 
 def binary_search(books, *, key, target):
+    """Binary search algorithm implementation for books.
+    
+    key must be either isbn or title.
+    """
     target = target.lower()
     left = 0
     right = len(books) - 1
