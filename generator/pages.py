50c8669285637fa64f873a9db3bfba9811d8ef37
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-04-02 10:44:03 +0300

    Refactor pages generator

--- generator/pages.py
+++ generator/pages.py
@@ -1,12 +1,9 @@
 from random import randrange
 
-try:
-    from cheryl.config import MIN_PAGES, MAX_PAGES
-except ModuleNotFoundError:
-    MIN_PAGES = 1
-    MAX_PAGES = 9999
+MIN_PAGES = 50
+MAX_PAGES = 1000
 
 
 def get_pages_list(n):
-    result = [randrange(MIN_PAGES, MAX_PAGES) for _ in range(n)]
-    return result
+    pages = [randrange(MIN_PAGES, MAX_PAGES) for _ in range(n)]
+    return pages
