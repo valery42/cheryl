b76f557c0aca289f4cbe8a64a57c2f06fe057b5c
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-04-02 10:52:58 +0300

    Document title module

--- generator/title.py
+++ generator/title.py
@@ -1,3 +1,5 @@
+"""This module contains title generator."""
+
 from random import choice
 
 FIRST = [
@@ -41,6 +43,7 @@ THIRD = [
 
 
 def get_title_list(n):
+    """Generate list of n title values."""
     titles = []
     for _ in range(n):
         title = choice(FIRST) + " " + choice(SECOND) + " " + choice(THIRD)
