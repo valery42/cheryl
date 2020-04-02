82f9b79f870652f496d940c0138b81e40a8d1cd1
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-04-02 10:48:06 +0300

    Document publisher module

--- generator/publisher.py
+++ generator/publisher.py
@@ -1,7 +1,10 @@
+"""This module contains publisher generator."""
+
 from random import choice
 from string import ascii_uppercase as UPPER
 
 
 def get_publisher_list(n):
+    """Generate list of n publisher values."""
     publisher = [choice(UPPER) + choice(UPPER) + " Press" for _ in range(n)]
     return publisher
