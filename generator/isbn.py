704e80da515621178b8039d68481883d4c8b1a6e
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-04-02 11:04:21 +0300

    Refactor isbn module

--- generator/isbn.py
+++ generator/isbn.py
@@ -1,12 +1,10 @@
 from itertools import product
 
-try:
-    from cheryl.config import ISBN_SEP
-except ModuleNotFoundError:
-    ISBN_SEP = "-"
+from cheryl.config import ISBN_SEP
 
 DIGITS = [str(i) for i in range(10)]
 ISBN_LIMIT = 1_000_000_000
+ISBN_DEFAULT_VALUE = ("000",)
 
 
 def get_isbn_values(n, first, second, third, *, sep=""):
@@ -22,10 +20,10 @@ def get_isbn_list(n):
     error_msg = f"n must be positive integer less than {ISBN_LIMIT}"
     assert n > 0 and n <= ISBN_LIMIT, error_msg
     if n <= 1_000:
-        first = second = ("000",)
+        first = second = ISBN_DEFAULT_VALUE
         third = get_isbn_values(n, DIGITS, DIGITS, DIGITS)
     elif n <= 1_000_000:
-        first = ("000",)
+        first = ISBN_DEFAULT_VALUE
         second = third = get_isbn_values(n, DIGITS, DIGITS, DIGITS)
     elif n <= ISBN_LIMIT:
         first = second = third = get_isbn_values(n, DIGITS, DIGITS, DIGITS)
