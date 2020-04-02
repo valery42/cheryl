d7c71dad4554f29bbcdeb104bb6082d444608bbf
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-04-02 10:45:30 +0300

    Document pages module

--- generator/pages.py
+++ generator/pages.py
@@ -1,3 +1,5 @@
+"""This module contains pages generator."""
+
 from random import randrange
 
 MIN_PAGES = 50
@@ -5,5 +7,6 @@ MAX_PAGES = 1000
 
 
 def get_pages_list(n):
+    """Generate list of n pages values."""
     pages = [randrange(MIN_PAGES, MAX_PAGES) for _ in range(n)]
     return pages
