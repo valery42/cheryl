3b976aea2e13dbc719d219bea6e6ec5b92409ffe
Valery Krivenkin <valery.krivenkin@gmail.com>
2020-04-02 10:47:23 +0300

    Refactor publisher generator

--- generator/publisher.py
+++ generator/publisher.py
@@ -3,8 +3,5 @@ from string import ascii_uppercase as UPPER
 
 
 def get_publisher_list(n):
-    result = []
-    for _ in range(n):
-        publisher = choice(UPPER) + choice(UPPER) + " Press"
-        result.append(publisher)
-    return result
+    publisher = [choice(UPPER) + choice(UPPER) + " Press" for _ in range(n)]
+    return publisher
